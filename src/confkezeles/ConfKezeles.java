28425fee2ca2ab1f666b3d1765854859d0622b39
T. Virág <78853394+virmoc@users.noreply.github.com>
2022-03-11 14:49:24 +0100

    nev2 szerk

--- src/confkezeles/ConfKezeles.java
+++ src/confkezeles/ConfKezeles.java
@@ -2,7 +2,7 @@ package confkezeles;
 public class ConfKezeles {
     public static void main(String[] args) {
         String nev1 = "TV";
-        String nev2;
+        String nev2 = "X.Y";
     }
     
 }
